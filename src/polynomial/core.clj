7ef6b1c0468bf35c22a6b3c072d402884b47a58c
Santiago Gepigon <santiago.gepigon@gmail.com>
2018-07-15 12:45:42 -0400

    Rename spec namespaces to Clojure convention

--- src/polynomial/core.clj
+++ src/polynomial/core.clj
@@ -1,11 +1,11 @@
 (ns polynomial.core
-  (:require [clojure.spec.alpha :as spec]
-            [clojure.spec.gen.alpha :as spec.gen]
-            [clojure.spec.test.alpha :as spec.test]
+  (:require [clojure.spec.alpha :as s]
+            [clojure.spec.gen.alpha :as sgen]
+            [clojure.spec.test.alpha :as stest]
             [expound.alpha :as expound]))
 
-(spec/def ::real (spec/double-in :infinite? false :NaN? false))
-(spec/def ::int-or-real (spec/or :int int? :real ::real))
+(s/def ::real (s/double-in :infinite? false :NaN? false))
+(s/def ::int-or-real (s/or :int int? :real ::real))
 
 (spec/def ::infix (spec/cat :x #{'x}
                             :op #{'+ '-}
@@ -23,11 +23,10 @@
 (spec/def ::polynomial (spec/cat :op #{'*}
                                  :coefficient ::int-or-real
                                  :expr ::expr))
-
-(spec/fdef polynomial->fn
-  :args (spec/cat :polynomial (spec/spec ::polynomial))
-  :ret (spec/fspec :args (spec/cat :x ::real)
-                   :ret number?))
+(s/fdef polynomial->fn
+  :args (s/cat :polynomial (s/spec ::polynomial))
+  :ret (s/fspec :args (s/cat :x ::real)
+                :ret number?))
 
 (defn polynomial->fn
   "convert a polynomial string to a Clojure function."
@@ -41,10 +40,10 @@
 
   ;; check previously failing seeds
   (for [seed [1531450911777 1531452083625 1531452439163]]
-    (spec.test/check `polynomial->fn {:clojure.spec.test.check/opts {:seed seed}}))
+    (stest/check `polynomial->fn {:clojure.spec.test.check/opts {:seed seed}}))
 
-  (set! spec/*explain-out* (expound/custom-printer {:show-valid-values? true
-                                                    :print-specs? false
-                                                    :theme :figwheel-theme}))
+  (set! s/*explain-out* (expound/custom-printer {:show-valid-values? true
+                                                 :print-specs? false
+                                                 :theme :figwheel-theme}))
   ;; Put your cursor on start of this line (after the above sexp) and type `SPC m e e`
   )
